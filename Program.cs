70f0db94246701d2fe9c514a38d62a7f942038dc
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-10-18 19:18:03 -0300

    Definições das variáveis e inicio do construtor

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ namespace graph_algorithm
     {
         static void Main(string[] args)
         {
-            Veiculo<int> veiculo = new Veiculo<int>(1, 2, 3, 2, (char)Direction.Horizontal);
+            Veiculo<int> veiculo = new Veiculo<int>(1, 2, 3, 2, Direction.Horizontal);
             Console.WriteLine(veiculo);
+            Console.WriteLine(veiculo.Diretion == Direction.Vertical);
         }
     }
 
@@ -16,7 +17,19 @@ namespace graph_algorithm
     {
         private Estado<T> atual;
         private int hight;
+        
+        private Veiculo<T> veiculo_start;
+        private Tuple<int, int> end;
 
+        public Graph(List<Veiculo<T>> veiculos, Veiculo<T> start, Tuple<int, int> end_position)
+        {
+            this.atual = new Estado<T>(veiculos);
+            this.hight = 0;
+            this.veiculo_start = start;
+            this.end = end_position;
+        }
+
+        
 
     }
 }
