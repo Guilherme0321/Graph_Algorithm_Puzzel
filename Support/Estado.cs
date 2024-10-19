8d5abf34e0f5672401b95f4d07ca36d915d8c68e
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-10-18 23:01:53 -0300

    setters e getters atualizados e sobrecarga de operador funcionando

--- Support/Estado.cs
+++ Support/Estado.cs
@@ -15,44 +15,71 @@ namespace graph_algorithm.Support
 
     class Estado<T>
     {
-        public Veiculo<T>? moved_veiculo;
-        public List<Veiculo<T>> veiculos;
+        private Veiculo<T>? movedVeiculo;
+        private List<Veiculo<T>> veiculos;
+        private List<Tuple<int, Estado<T>>> edges;
 
-        public List<Tuple<int, Estado<T>>>? edges;
-
-        public Estado()
+        public Estado(Veiculo<T> moved, List<Veiculo<T>> list)
         {
-            veiculos = null;
-            moved_veiculo = null;
-            edges = null;
-        }
-        public Estado(Veiculo<T> moved)
-        {
-            this.moved_veiculo = moved;
-            this.veiculos = new List<Veiculo<T>>();
+            this.movedVeiculo = moved;
+            this.veiculos = new List<Veiculo<T>>(list);
             this.edges = new List<Tuple<int, Estado<T>>>();
         }
 
         public Estado(List<Veiculo<T>> list)
         {
             this.veiculos = new List<Veiculo<T>>(list);
-            this.moved_veiculo = null;
+            this.movedVeiculo = null;
             this.edges = new List<Tuple<int, Estado<T>>>();
         }
 
-        private bool Equals(Estado<T> obj)
+        public Veiculo<T> MovedVeiculo { get { return this.movedVeiculo; } set { this.movedVeiculo = value; } }
+        public List<Veiculo<T>> Veiculos { get { return this.veiculos; } }
+        public List<Tuple<int, Estado<T>>> Edges { get { return this.edges; } }
+
+        public void setVeiculoAt(int index, Veiculo<T> tmp)
         {
-            if(obj == null) return false;
-            for(int i = 0; i < this.veiculos.Count; i++) 
+            if(index >= 0 && index < this.veiculos.Count)
             {
-                if (this.veiculos[i].EqualsPosition(obj.veiculos[i])) return false;
+                veiculos[index] = tmp;
+            } 
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "O índice está fora do intervalo do array de veículos.");
             }
+        }
+
+        private bool Equal(Estado<T> obj)
+        {
+            if (obj == null) return false;
+
+            if (this.veiculos.Count != obj.veiculos.Count) return false;
+
+            for (int i = 0; i < this.veiculos.Count; i++)
+            {
+                if (!veiculos[i].EqualsPosition(obj.veiculos[i])) return false;
+            }
+
             return true;
         }
 
-        public static bool operator ==(Estado<T> i, Estado<T> j) { return i.Equals(j); }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+            return Equal((Estado<T>)obj);
+        }
 
-        public static bool operator !=(Estado<T> i, Estado<T> j) { return !(i == j); }
+        public static bool operator ==(Estado<T> i, Estado<T> j)
+        {
+            if (ReferenceEquals(i, null) && ReferenceEquals(j, null)) return true;
+            if (ReferenceEquals(i, null) || ReferenceEquals(j, null)) return false;
+            return i.Equals(j);
+        }
+
+        public static bool operator !=(Estado<T> i, Estado<T> j)
+        {
+            return !(i == j);
+        }
 
         public override int GetHashCode()
         {
