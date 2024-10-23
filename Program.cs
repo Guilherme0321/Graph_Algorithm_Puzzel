7e361d75d6c5cd78518c0ec282a8c7259374e007
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-10-23 20:08:00 -0300

    função para pegar a heuristica e iniciando a hash

--- Program.cs
+++ Program.cs
@@ -12,12 +12,8 @@ namespace graph_algorithm
             Veiculo<int> v2 = new Veiculo<int>(2, 1, 0, 3, Direction.Vertical, false); 
             Veiculo<int> v3 = new Veiculo<int>(3, 0, 1, 2, Direction.Horizontal, true); // obstáculo
 
-            // Criação do estado inicial
-            List<Veiculo<int>> initialVehicles = new List<Veiculo<int>>() { v1, v2, v3 };
-            Estado<int> initialState = new Estado<int>(initialVehicles);
-
             // Movendo o veículo 1 para a direita
-            v1.MoveHorizontally(1, Veiculo<int>.MoveHorizontal.Direita);
+            v1 = v1.MoveHorizontally(1, MoveHorizontal.Direita);
 
             // Criação do novo estado após mover o veículo 1
             List<Veiculo<int>> movedVehicles = new List<Veiculo<int>>() { v1, v2, v3 };
@@ -44,15 +40,50 @@ namespace graph_algorithm
         private Veiculo<T> veiculo_start;
         private Tuple<int, int> end;
 
+        private Dictionary<Estado<T>, bool> hash;
+
         public Graph(List<Veiculo<T>> veiculos, Veiculo<T> start, Tuple<int, int> end_position)
         {
             this.atual = new Estado<T>(veiculos);
             this.hight = 0;
             this.veiculo_start = start;
             this.end = end_position;
+
+            hash = new Dictionary<Estado<T>, bool>()
+            {
+                { atual, true }
+            };
         }
 
-        
+        public bool HasBeenAnalysed(Estado<T> estado)
+        {
+            return hash[estado];
+        }
+
+        public int CalcularHeuristica(Veiculo<T> carroVermelho, List<Veiculo<T>> outrosVeiculos, Tuple<int, int> posicaoSaida)
+        {
+            int heuristica = 0;
+
+            int xAtual = carroVermelho.Position.Item1;
+            int yAtual = carroVermelho.Position.Item2;
+
+            int xFinal = posicaoSaida.Item1;
+            int yFinal = posicaoSaida.Item2;
+
+            heuristica += Math.Abs(xAtual - xFinal) + Math.Abs(yAtual - yFinal);
+
+            // Considera os veículos que estão bloqueando o caminho do carro vermelho
+            foreach (var veiculo in outrosVeiculos)
+            {
+                if (veiculo.Position.Item2 == carroVermelho.Position.Item2 && veiculo.Position.Item1 > carroVermelho.Position.Item1)
+                {
+                    heuristica += 1;
+                }
+            }
+
+            return heuristica;
+        }
+
 
     }
 }
